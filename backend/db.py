6e69c9ecc2cf14d19641b745a190c4e6f03cbe2a
Tal Kahila <talka4444@gmail.com>
2025-09-14 23:07:31 +0300

    docket compose changes

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from suites import suites
+import os
 
-DATABASE_URL = "sqlite:///./tenet.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///./tenet.db"), connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
