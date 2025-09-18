3377cf0ac3427b28bf90a458e1e2ca1f67f883e7
Tal Kahila <talka4444@gmail.com>
2025-09-18 12:47:38 +0300

    minor change

--- backend/tasks.py
+++ backend/tasks.py
@@ -7,24 +7,24 @@ fake = Faker()
 
 celery_app = Celery(
     "tasks",
-    broker=f"{os.getenv("REDIS_URL", "redis//localhost:6379")}/0",
-    backend=f"{os.getenv("REDIS_URL", "redis//localhost:6379")}/0",
+    broker=f"{os.getenv("REDIS_URL", "redis://localhost:6379")}/0",
+    backend=f"{os.getenv("REDIS_URL", "redis://localhost:6379")}/0",
 )
 
 @celery_app.task
 def run_prompt(chat_url: str, prompt: str):
-    payload = {
+    data = {
         "name": fake.first_name(),
         "email": fake.email(),
         "message": prompt,
         "terms_accepted": True,
         "marketing_opt_in": True,
         "chat_history": [],
-        "session_id": ""
+        "session_id": "session"
     }
 
     try:
-        bot_response = requests.post(chat_url, json=payload)
+        bot_response = requests.post(chat_url, json=data)
         bot_response.raise_for_status()
         response = bot_response.json()
 
